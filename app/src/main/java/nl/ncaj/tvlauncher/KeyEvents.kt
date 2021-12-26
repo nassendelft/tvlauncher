5c2e45b9cd8fa2186fd41def80037c11493bfd57
Nick Assendelft <n.assendelft@gmail.com>
2021-12-26 14:26:05 +0100

    [Fix] Fixes selection app with remote (#4)

--- app/src/main/java/nl/ncaj/tvlauncher/KeyEvents.kt
+++ app/src/main/java/nl/ncaj/tvlauncher/KeyEvents.kt
@@ -3,8 +3,13 @@ package nl.ncaj.tvlauncher
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.*
 
+private val acceptKeys = listOf(
+  Key.Enter,
+  Key.DirectionCenter
+)
+
 /**
- * Calls [onEnter] when the key pressed is of [Key.Enter]
+ * Calls [onEnter] when the key pressed is of one of [acceptKeys]
  * and the event is of [KeyEventType.KeyUp].
  *
  * @param onEnter the lambda that is called when the above criteria is met
@@ -12,7 +17,7 @@ import androidx.compose.ui.input.key.*
 fun Modifier.onEnterKeyEvent(
   onEnter: () -> Unit
 ) = this.onKeyEvent {
-  if (it.type == KeyEventType.KeyUp && it.key == Key.Enter) {
+  if (it.type == KeyEventType.KeyUp && acceptKeys.contains(it.key)) {
     onEnter()
     return@onKeyEvent true
   }
